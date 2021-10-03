1322793ff07044bc311f96bd319f5921ae880991
Vivek <vivekyadav6838@gmail.com>
2021-10-03 14:39:32 +0530

    if understood

---
+++ ifinpython.py
@@ -0,0 +1,9 @@
+# python program to print a man is elegible for or not
+print("enter you age")
+age =  int(input())
+name = input("enter your name")
+if(age>=18):
+    print("YES!!" + name + " you are elgible to vote")
+else:
+    print("No!!" + name + " you are not elgible to vote")
+print("Have a nice DAY!!! "+ name)
